6b677702a39664e989e3ba07ae1aae44990e18b9
Daniela Schalldach <sulikova.daniela@gmail.com>
2023-05-27 17:41:04 +0200

    state before handleBuy

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { JourneyPicker } from '../JourneyPicker';
 import { JourneyDetail } from "../JourneyDetail";
+import { SelectedSeat } from "../SelectedSeat";
+import { useNavigate } from "react-router-dom";
 // import { useState } from 'react';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
+  const navigate=useNavigate();
 
   const handleJourneyChange = (journey) => {
     setJourney(journey);
+  };
+  const handleBuy=()=> {
+console.log("Funguju")
   };
 
   return (
@@ -17,7 +23,12 @@ export const Home = () => {
         <div className="container">
           Nalezeno spojení s id 
           <JourneyDetail journey={journey}/>
+          <SelectedSeat number={journey.autoSeat}/>
           {/* {journey.journeyId} */}
+          <div className="controls container">
+      <button className="btn btn--big" type="button" onClick={handleBuy()}>Rezervovat</button>
+    </div>
+
         </div>
       )}
     </main>
